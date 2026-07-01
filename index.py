a18105e27ff0bdb4285271836b53e4fb4582efde
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-01 21:05:33 +0200

    Starting

---
+++ index.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+
+print("This is a sample Python script."); print("It demonstrates basic syntax and functionality.")
+
+print('Hello World!') #In Python both single and double quotes can be used to define strings.
+
