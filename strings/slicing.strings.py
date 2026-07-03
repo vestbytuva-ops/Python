a491ba492faf5cf72eba1847d6c1119e025c7739
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-03 19:44:34 +0200

    Slicing Strings

---
+++ strings/slicing.strings.py
@@ -0,0 +1,20 @@
+b="Hello World!"
+print(b[2:5]) #Get the characters from position 2 to position 5 (not included):
+
+#Slice From the Start:
+
+b="Hello World!"
+print(b[:5])
+
+
+#Slice To the End:
+
+b="Hello World!"
+print(b[2:])
+
+
+#Negative Indexing:
+
+b="Hello World"
+print(b[-5:-2])
+
