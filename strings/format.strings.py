7331207af5daed51c359c08d23efd010e0914be2
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-05 10:23:49 +0200

    Format Strings

--- strings/format.strings.py
+++ strings/format.strings.py
@@ -3,3 +3,21 @@
 age=36
 txt = f"My name is John, I am {age}"
 print(txt)
+
+#Placeholder and Modifiers
+
+price = 59
+txt=f"The price is {price} dollars"
+print(txt)
+
+#Displays the price with 2 decimals:
+
+price = 59
+txt=f"The price is {price:.2f} dollars"
+print(txt)
+
+#Math operations:
+
+txt= f"The price is {20*59} dollars"
+print(txt)
+
