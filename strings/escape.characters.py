29fbe818b2c5c9c75368baedb6831b348e6d56af
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-05 10:27:42 +0200

    Escape Characters

---
+++ strings/escape.characters.py
@@ -0,0 +1,15 @@
+#Escape character
+
+"""
+You will get an error if you use double quotes inside a string that is surrounded by doble quotes:
+
+txt="We are the so-called "Vikings" from the north."
+
+To fix this problem, use the escape character \":
+"""
+
+#Example:
+
+txt="We are the so-called \"Vikings\" from the north."
+
+
