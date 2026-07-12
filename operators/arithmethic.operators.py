a78f27da5c3ff3e661abb83bf4cbf71d52e5117d
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-12 20:45:39 +0200

    Python Operators

---
+++ operators/arithmethic.operators.py
@@ -0,0 +1,34 @@
+#Example:
+
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+
+#Division in Python
+
+x=12
+y=5
+
+print(x/y)
+
+#This returns an Float
+
+
+#Example:
+
+x=12
+y=5
+
+print(x//y)
+
+#This returns an Integers and rounds DOWN to the nearest integer.
+
+
---
+++ operators/python.operators.py
@@ -0,0 +1,18 @@
+#Python Operators:
+
+print(10+5)
+
+"""
+Although the + operator is often used to add together two values, 
+like in the example above, it can also be used to add together a 
+variable and a value, or two variables:
+"""
+
+#Example:
+
+sum1 = 100 + 50 
+sum2 =  sum1 + 250
+sum3 = sum2 + sum2
+
+
+
