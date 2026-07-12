40e00cb595292d3332f719ae4678eaedcc351dd2
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-12 20:08:50 +0200

    Booleans Code Challenge

---
+++ booleans/booleans.challenge.py
@@ -0,0 +1,20 @@
+"""
+Inside the editor, complete the following steps:
+Print the result of 10 > 9
+Print the result of 10 == 9
+Print the result of bool("Hello")
+Print the result of bool(0)
+
+"""
+
+# Print the result of 10 > 9
+print(10>9)
+
+# Print the result of 10 == 9
+print(10==9)
+
+# Print the result of bool("Hello")
+print(bool("Hello"))
+
+# Print the result of bool(0)
+print(bool(0))
