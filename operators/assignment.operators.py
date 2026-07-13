20d95d6e3e53b48b72b033fb066d38272e4d8507
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-13 20:59:46 +0200

    Assignment Operators

---
+++ operators/assignment.operators.py
@@ -0,0 +1,10 @@
+#The count variable is assigned in the if statement, and given the value 5:
+
+numbers = [1,2,3,4,5]
+
+if (count :=len(numbers)) > 3:
+    print(f"List has {count} elements")
+
+"""
+Output is: List has 5 elements
+"""
