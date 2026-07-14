2582c6968514a14da1b30a485f859780d92ebdb3
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-14 20:46:44 +0200

    Logical Operators

---
+++ operators/logical.operators.py
@@ -0,0 +1,30 @@
+#Python Logical Operators:
+
+#Example:
+
+x = 5
+
+print(x > 0 and x < 10)
+
+"""
+Test if a number is greater than 0 and less than 10:
+"""
+#Example:
+
+x = 5
+
+print(x < 5 or x > 10)
+
+"""
+Test if a number is less than 5 or greater than 10:
+"""
+
+#Example:
+
+x = 5
+
+print(not(x > 3 and x < 10))
+
+"""
+Reverse the result with not:
+"""
