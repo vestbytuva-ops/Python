7237ca5e5e36f8e675e1c0a92e4a1ed715bc4b91
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-14 20:43:14 +0200

    Comparison Operators

---
+++ operators/comparison.operators.py
@@ -0,0 +1,19 @@
+#Comparison Operators:
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Chaining Comparison Operators:
+
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
