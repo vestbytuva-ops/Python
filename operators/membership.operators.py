d84ab5e3fb8c6d018a2484d5dee6d33566d27cc2
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-15 22:10:44 +0200

    Membership Operators

---
+++ operators/membership.operators.py
@@ -0,0 +1,32 @@
+#Membership Operators:
+
+#Example:
+
+fruits = ["apple", "banana", "cherry"]
+
+print("banana" in fruits)
+
+#Check if "banana" is present in a list:
+
+#Example:
+
+fruits = ["apple", "banana", "cherry"]
+
+print("pineapple" not in fruits)
+
+#Check if "pineapple" is NOT present in a list:
+
+#Membership in Strings:
+
+text = "Hello World"
+
+print("H" in text)
+print("hello" in text)
+print("z" not in text)
+
+"""
+Output:
+True
+False
+True
+"""
