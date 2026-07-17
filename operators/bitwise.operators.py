75bec4a105d0c470c5d4d4ae40ef05d858ee55f0
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-17 11:42:05 +0200

    Bitwise Operators

---
+++ operators/bitwise.operators.py
@@ -0,0 +1,32 @@
+#Bitwise Operators:
+
+#Example:
+
+#The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0:
+
+print(6 & 3)
+#The binary representation of 6 is 0110
+#The binary representation of 3 is 0011
+
+#Then the & operator compares the bits and returns 0010, which is 2 in decimal.
+
+#Example:
+
+#The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+
+print(6 | 3)
+#The binary representation of 6 is 0110
+#The binary representation of 3 is 0011
+
+#Then the | operator compares the bits and returns 0111, which is 7 in decimal.
+
+#Example:
+
+#The ^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0:
+
+
+print(6 ^ 3)
+#The binary representation of 6 is 0110
+#The binary representation of 3 is 0011
+
+#Then the ^ operator compares the bits and returns 0101, which is 5 in decimal.
