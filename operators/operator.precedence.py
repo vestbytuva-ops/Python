71fd0afaee6be80e2153d32b385256a4ea836b8e
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-18 11:26:35 +0200

    Operator Precedence

---
+++ operators/operator.precedence.py
@@ -0,0 +1,19 @@
+#Python Operator Precedence:
+
+"""
+Operator precedence describes the order in which operations are performed.
+"""
+
+#Example:
+
+print((6+3)-(6+3))
+
+
+#Example:
+
+print(100+5*3)
+
+
+#Example:
+
+print(5 + 4 - 7 + 3)
