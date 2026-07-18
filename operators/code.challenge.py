0fcc6065d825668a7be596420c7bd99f35697d20
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-18 11:32:39 +0200

    Code Challenge Operators

---
+++ operators/code.challenge.py
@@ -0,0 +1,17 @@
+#Challenge: Operators Basics
+
+# Create variables
+a = 15
+b = 4
+
+# Print modulus
+print(a % b)
+
+# Print floor division
+print(a//b)
+
+# Print power
+print(a**b)
+
+# Add 10 to a
+a += 10
