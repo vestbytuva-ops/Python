3d61f0ef73d97d236c7bf210f7a4319a139a0a9f
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-20 22:10:03 +0200

    Change List Items

---
+++ lists/change.list.items.py
@@ -0,0 +1,9 @@
+#Python - Change List Items:
+
+"""
+To change the value of a specific item, refer to the index number:
+"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
