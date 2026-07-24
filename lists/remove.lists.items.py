5bfa744366db3af97af48323e426ef825799f3f8
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-24 11:47:23 +0200

    Remove Specified Index

--- lists/remove.lists.items.py
+++ lists/remove.lists.items.py
@@ -25,3 +25,36 @@ print(thislist)
 
 #Remove Specified Index:
 
+#Example:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1) #The pop() method removes the specified index.
+print(thislist)
+
+#Example:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop() #If you do not specify the index, the pop() method removes the last item.
+print(thislist)
+
+#Example
+
+"""
+The del keyword also removes the specified index:
+"""
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0] #Removes the first item.
+print(thislist)
+
+
+#Example
+
+thislist = ["apple", "banana", "cherry"]
+del thislist #Delete the entire list.
+
+"""If i want to print this it will cause an error because the list is deleted and there is nothing to print."""
+
+
+
+
