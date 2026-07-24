bf590f00659f02f917a96b3d9320155bbc04020c
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-24 11:42:22 +0200

    Remove List Items

---
+++ lists/remove.lists.items.py
@@ -0,0 +1,27 @@
+#Remove List Items:
+
+"""
+Remove Specified Item
+
+The remove() method removes the specified item.
+"""
+
+#Example:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
+#Example: 
+"""
+Remove the first occurrence of "banana" 
+"""
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+
+#Remove Specified Index:
+
