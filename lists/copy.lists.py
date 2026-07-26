0a6bf395cab2e9a1df9ba606f9fe14dbba12063b
vestbytuva-ops <vestby.tuva@gmail.com>
2026-07-26 17:51:28 +0200

    Copy Lists

---
+++ lists/copy.lists.py
@@ -0,0 +1,18 @@
+#Python Copy Lists:
+
+"""Use the copy() method"""
+
+#Example: Make a copy of a list with the copy() method.
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+"""Use the slice Operator"""
+
+#Example: Make a copy of a list with the : operator.
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
