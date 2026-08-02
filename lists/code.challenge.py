d9b6e131dab640c07df86b3df5ba103eb594f27a
vestbytuva-ops <vestby.tuva@gmail.com>
2026-08-02 19:23:13 +0200

    Code Challenge

---
+++ lists/code.challenge.py
@@ -0,0 +1,14 @@
+"""Python Code Challenge"""
+
+# Create a list
+colors = ["red", "green", "blue"]
+# Print the first item
+print(colors[0])
+# Change the second item to "yellow"
+colors[1]="yellow"
+# Add "purple" to the end
+colors.append("purple")
+# Remove "red"
+colors.remove("red")
+# Print the list
+print(colors)
