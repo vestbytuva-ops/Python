ec82b535eac338f582d95dea72694a8695063626
vestbytuva-ops <vestby.tuva@gmail.com>
2026-08-09 21:23:39 +0200

    CHANGES: Fixing the code and imported a type of morse code dictionary.

--- projects/morse.py
+++ projects/morse.py
@@ -1,12 +1,28 @@
-import morse
+    MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                        'C':'-.-.', 'D':'-..', 'E':'.',
+                        'F':'..-.', 'G':'--.', 'H':'....',
+                        'I':'..', 'J':'.---', 'K':'-.-',
+                        'L':'.-..', 'M':'--', 'N':'-.',
+                        'O':'---', 'P':'.--.', 'Q':'--.-',
+                        'R':'.-.', 'S':'...', 'T':'-',
+                        'U':'..-', 'V':'...-', 'W':'.--',
+                        'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                        '1':'.----', '2':'..---', '3':'...--',
+                        '4':'....-', '5':'.....', '6':'-....',
+                        '7':'--...', '8':'---..', '9':'----.',
+                        '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                        '?':'..--..', '/':'-..-.', '-':'-....-',
+                        '(':'-.--.', ')':'-.--.-'}
 
-decoded = input("Enter your sentence")
 
-#print(f"Are you sure you want to transfer this sentence into morse, {decoded}")
+    sentence = input("Enter your sentence")
 
-confirmation = input(f"Are you sure you want to transfer this sentence into morse?, {decoded} (Y / N)")
+    #print(f"Are you sure you want to transfer this sentence into morse, {decoded}")
 
-if confirmation.upper() == "Y":
-    print("Here is your sentence converted into morse:")
-else:
-    print("Script is cancelled by user")
+    confirmation = input(f"Are you sure you want to transfer this sentence into morse?, {sentence} (Y / N)")
+
+    if confirmation.upper() == "Y":
+        sentence.upper() == new_sentence
+        print("Here is your sentence converted into morse:")
+    else:
+        print("Script is cancelled by user")
