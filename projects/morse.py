1e7fbc39c66c53425f861419c8ee417f6cb6b0c0
vestbytuva-ops <vestby.tuva@gmail.com>
2026-08-09 21:07:26 +0200

    CHANGES: importing morse

--- projects/morse.py
+++ projects/morse.py
@@ -2,5 +2,11 @@ import morse
 
 decoded = input("Enter your sentence")
 
-print(f"Are you sure you want to transfer this sentence into morse, {decoded}")
+#print(f"Are you sure you want to transfer this sentence into morse, {decoded}")
 
+confirmation = input(f"Are you sure you want to transfer this sentence into morse?, {decoded} (Y / N)")
+
+if confirmation.upper() == "Y":
+    print("Here is your sentence converted into morse:")
+else:
+    print("Script is cancelled by user")
