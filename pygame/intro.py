a214fe954a9d9157e417578e5bcac30d7fee09b2
vestbytuva-ops <vestby.tuva@gmail.com>
2026-08-31 19:28:00 +0200

    CHANGES: created new map and player color

--- pygame/intro.py
+++ pygame/intro.py
@@ -5,34 +5,32 @@ p.init()
 screen_width = 800
 screen_height = 600
 
-screen = p.display.set_mode((screen_width,screen_height))
+screen = p.display.set_mode((screen_width, screen_height))
+p.display.set_caption("Pygame")
 
-player = p.Rect((300,250,50,50))
+player = p.Rect((300, 250, 50, 50))
 
 run = True
+
 while run:
 
-    p.draw.rect(screen, (255,0,0), player)
+    screen.fill((245, 245, 220))  # clear the screen each frame
+    p.draw.rect(screen,(173, 216, 230) , player)
 
     key = p.key.get_pressed()
-    if key[p.K_a] == True:
-        player.move_ip(-1,0)
-    key = p.key.get_pressed()
-    if key[p.K_d] == True:
-        player.move_ip(1,0)
-    key = p.key.get_pressed()
-    if key[p.K_w] == True:
-        player.move_ip(0,-1)
-    key = p.key.get_pressed()
-    if key[p.K_s] == True:
-        player.move_ip(0,1)
+    if key[p.K_a]:
+        player.move_ip(-1, 0)
+    if key[p.K_d]:
+        player.move_ip(1, 0)
+    if key[p.K_w]:
+        player.move_ip(0, -1)
+    if key[p.K_s]:
+        player.move_ip(0, 1)
 
-for event in p.event.get():
-    if event.type == p.quit:
-        run = False
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            run = False
 
     p.display.update()
 
 p.quit()
-
-
