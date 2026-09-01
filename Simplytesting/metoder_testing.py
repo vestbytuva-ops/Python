61d6d235b49d7e1651acdf5b6bdb61a7b8bddf89
SimplyHero-dev <eric07.tran@gmail.com>
2026-09-01 08:08:08 +0200

    CHANGES: made "metoder_testing" slightly better

--- Simplytesting/metoder_testing.py
+++ Simplytesting/metoder_testing.py
@@ -1,5 +1,9 @@
 text = "Tuvarakan er en svart DIGGER"
 
+print(text.replace("D", "N"))   #Bytter ut D med N
+
+text = text.replace("D", "N")
+
 print(text.upper())             #Gjør alt til store bokstaver
 
 print(text.lower())             #Gjør alt til små bokstaver
@@ -7,8 +11,6 @@ print(text.lower())             #Gjør alt til små bokstaver
 print(text.capitalize())        #Første bokstav blir stor, de andre blir små
 
 print(text.title())             #Første bokstav i hver ord blir stor, resten bli små
-
-print(text.replace("D", "N"))   #Bytter ut D med N
 
 print(text.index("G"))          #Finner indeks til første G. Starter å telle på null (blir 24)
 
