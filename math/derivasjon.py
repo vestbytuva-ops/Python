e7b91caad7b7465a205511970a0ec3b99481f3c3
vestbytuva-ops <vestby.tuva@gmail.com>
2026-09-08 19:32:27 +0200

    REFINE: updated function f to calculate cubic values and adjusted input for snittfart

--- math/derivasjon.py
+++ math/derivasjon.py
@@ -1,7 +1,10 @@
 def f(x):
-    return 0.06*x**2 + 4*x - 7
+    return 2*x**3
 
 def snittfart (x1, x2):
     return (f(x2)-f(x1))/(x2-x1)
-snitt = round(snittfart(-1,3),2)
+
+x0 = 3
+
+snitt = round(snittfart(x0-1/2,x0+1/2),2)
 print(f"Dette er den gjennomsnittlig momentanfarten til funksjonen {snitt}")
