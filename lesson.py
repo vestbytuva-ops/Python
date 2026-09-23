6f2f6e69ca8458e285549888c9e09cc2f58eed54
vestbytuva-ops <vestby.tuva@gmail.com>
2026-09-23 17:43:45 +0200

    Refactor arithmetic functions and enhance mail-related functionalities

--- lesson.py
+++ lesson.py
@@ -15,13 +15,90 @@ def substract(x,y):
     z = x - y
     return z
 
-def substract(x,y):
+def multi(x,y):
     z = x * y
     return z
 
-def substract(x,y):
+def divide(x,y):
     z = x / y
     return z
 
 
 print(add(1,2))
+print(substract(1,2))
+print(multi(1,2))
+print(divide(1,2))
+
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("bro", "code")
+
+print(full_name)
+#
+
+def mail(navn, alder, by):
+    navn = navn.lower()
+    by = by.lower()
+    return navn + alder + by + "@gmail.com"
+
+mail_info = mail("test", "18" , "vestby")
+print(mail_info)
+
+
+
+
+def mail(navn, alder, sted):
+    navn = navn.lower()
+    sted = sted.lower()
+    return navn + alder + sted + "@gmail.com"
+
+
+navn = input("navn: ")
+alder = input("alder: ")
+sted = input("sted: ")
+
+mail_info = mail(navn, alder, sted)
+
+print(mail_info)
+
+#
+
+def fullt_navn(fornavn, etternavn):
+    fornavn = fornavn.lower()
+    etternavn = etternavn.lower()
+    return fornavn + " " + etternavn
+
+navn2 = fullt_navn("Ola", "Nordmann")
+
+print(navn2)
+
+#
+
+def bruker_info2(navn2, alder2, by2):
+    navn2 = navn2.lower()
+    by2 = by2.lower()
+    return navn2 + alder2 + by2
+
+info2 = bruker_info2("test", "18", "vestby")
+
+print(info2)
+
+#
+def lag_id(navn3, alder3):
+    navn3 = navn3.lower()
+    return navn3 + str(alder3)
+
+info3 = lag_id("ola", 18)
+print(info3)
+
+#
+
+def epost(navn, alder, sted):
+    navn.lower()
+    sted.lower()
+    return navn + alder + sted + "@gmail.com"
+
