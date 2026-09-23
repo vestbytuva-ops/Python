94152660156b7acc34cfa707ac348b64a031492d
vestbytuva-ops <vestby.tuva@gmail.com>
2026-09-23 17:43:37 +0200

    Add invoice display function and basic arithmetic operations

---
+++ lesson.py
@@ -0,0 +1,27 @@
+def display_invoice(username, amout, due_data):
+    print(f"hello {username}")
+    print(f"hello bill of {amout:.2f} is due: {due_data}")
+
+display_invoice("test", 100, "01/02")
+
+#
+
+def add(x,y):
+    z = x + y
+    return z
+
+
+def substract(x,y):
+    z = x - y
+    return z
+
+def substract(x,y):
+    z = x * y
+    return z
+
+def substract(x,y):
+    z = x / y
+    return z
+
+
+print(add(1,2))
