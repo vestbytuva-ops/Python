48606b6568d6ee4d4c6d491506803b986d08bc2d
SimplyHero-dev <eric07.tran@gmail.com>
2026-09-24 08:40:13 +0200

    changes in 2.25

--- math/240926.board.tasks.Eric.py
+++ math/240926.board.tasks.Eric.py
@@ -1,8 +1,22 @@
+#trapesmetode
 # from math import sqrt
 
 # def f(x):
 #     return sqrt(4 - x**2)
 
+# a = -2
+# b = 2
+# n = 5
+# dx = (b - a) / n
+# areal = 0
+
+# for i in range(n):
+#     areal = areal + (f(dx * i + a) + f(dx*(i + 1) + a)) / 2 * dx
+
+# print(areal)
+
+
+#2.25
 def f(x):
     return x**(2*x)
 
@@ -17,4 +31,3 @@ for i in range(n):
 print(areal)
 
 
-#(f(dx * i + a) + f(dx*(i + 1) + a)) / 2 * dx
