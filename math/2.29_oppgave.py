a4c23a170835467219866b59cb003ea838e4ee94
vestbytuva-ops <vestby.tuva@gmail.com>
2026-09-24 16:02:19 +0200

    Fix trapezoidal method calculation and correct function return values

--- math/2.29_oppgave.py
+++ math/2.29_oppgave.py
@@ -2,8 +2,9 @@ a=2
 b=1
 n=5
 
+
 def f(x):
-    return 1/(x*x**2)
+    return 1/(x+x**2)
 
 def trapes_metode(a,b,n):
     dx = (b-a)/n
@@ -11,12 +12,12 @@ def trapes_metode(a,b,n):
 
     for i in range(n):
         x1 = a + i * dx
-        x2 = a + (n+1) * dx
-        areal = dx(f(x1)-f(x2))/ 2 * dx
+        x2 = a + (i+1) * dx
+        areal = areal + (f(x1)-f(x2))/ 2 * dx
 
-    return a,b,n
+    return areal
 
-trapes_sum = trapes_metode(a,b,areal)
+trapes_sum = trapes_metode(a,b,n)
 print(trapes_sum)
 
 
